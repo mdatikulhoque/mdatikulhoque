34268277a8c93f3d93997978a309559f2cadd7a2
mdatikulhoque <mdatikul1136@gmail.com>
2025-09-06 18:33:23 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 80;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
